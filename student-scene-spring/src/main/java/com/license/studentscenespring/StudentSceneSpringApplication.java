f4a03c812248358bb305d85a445a4231e0c86bc8
Alex Bahrin <73994505+alexbhr13@users.noreply.github.com>
2024-04-08 13:44:12 +0300

    added spring boot and angular project

---
+++ student-scene-spring/src/main/java/com/license/studentscenespring/StudentSceneSpringApplication.java
@@ -0,0 +1,13 @@
+package com.license.studentscenespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSceneSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentSceneSpringApplication.class, args);
+    }
+
+}
