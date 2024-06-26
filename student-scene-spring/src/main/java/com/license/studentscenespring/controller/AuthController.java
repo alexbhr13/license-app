ee9fc135816ce7b90de8f19f979a882d92637c97
Alex Bahrin <73994505+alexbhr13@users.noreply.github.com>
2024-06-27 02:41:39 +0300

    final be commit

--- student-scene-spring/src/main/java/com/license/studentscenespring/controller/AuthController.java
+++ student-scene-spring/src/main/java/com/license/studentscenespring/controller/AuthController.java
@@ -2,12 +2,17 @@ package com.license.studentscenespring.controller;
 
 import com.license.studentscenespring.dto.AuthRequestDTO;
 import com.license.studentscenespring.dto.AuthResponseDTO;
+import com.license.studentscenespring.security.JWTService;
 import com.license.studentscenespring.service.AuthService;
 import com.license.studentscenespring.service.IAuthService;
+import com.license.studentscenespring.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.support.ETagDoesntMatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -21,6 +26,8 @@ public class AuthController {
 
 
     private final IAuthService authService;
+    private final JWTService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @PostMapping("/login")
     private ResponseEntity<Map<String,String>> loginUser(@RequestBody AuthRequestDTO request) {
@@ -40,5 +47,17 @@ public class AuthController {
         }
         response.put("message","login error");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    }
+
+    @GetMapping("/validate")
+    private ResponseEntity<Boolean> validateToken(@RequestHeader ("Authorization") String token) {
+        try{
+            UserDetails userDetails = userDetailsService.loadUserByUsername(jwtService.extractEmail(token));
+            boolean isTokenValid = jwtService.isTokenValid(token,userDetails);
+            if(!isTokenValid) throw new Exception();
+            return ResponseEntity.status(HttpStatus.OK).body(true);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
     }
 }
